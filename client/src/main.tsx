5ffc04a8a83a980f1bd71c4b70825914d4ce9364
Brendon Hudnell <bthudnell@gmail.com>
2021-04-12 12:45:38 -0600

    added redux, initial store, and kingdom module

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './components/store';
 import { ShellView } from './modules/shell';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<ShellView />
+		<Provider store={store}>
+			<ShellView />
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
