af668bdb77a906fefa08300e345f6cb9bf145709
Brendon Hudnell <bthudnell@gmail.com>
2021-05-11 15:50:18 -0600

    added functions to get bonuses from terrain improvements and special terrain

--- client/src/modules/hex/hexUtils.ts
+++ client/src/modules/hex/hexUtils.ts
@@ -1,4 +1,5 @@
-import { HexData } from './hexSlice';
+import { useAppSelector } from '../../components/store';
+import { HexData, selectClaimedHexes } from './hexSlice';
 
 export enum TerrainType {
 	CAVERN = 'Cavern',
@@ -45,6 +46,170 @@ export enum TerrainImprovementType {
 	ROAD = 'Road',
 	SAWMILL = 'Sawmill',
 	WATCHTOWER = 'Watchtower',
+}
+
+export function useClaimedHexesEconomyBonus(): number {
+	const claimedHexes = useAppSelector((state) => selectClaimedHexes(state));
+	let roadTotal = 0;
+	let mineTotal = 0;
+	let riverTotal = 0;
+	let resourceTotal = 0;
+
+	claimedHexes.forEach((hex) => {
+		if (hex.terrainImprovements.includes(TerrainImprovementType.MINE)) {
+			hex.specialTerrain.includes(SpecialTerrainType.RESOURCE)
+				? (mineTotal += 2)
+				: mineTotal++;
+		}
+		if (
+			hex.terrainImprovements.includes(TerrainImprovementType.ROAD) ||
+			hex.terrainImprovements.includes(TerrainImprovementType.HIGHWAY)
+		) {
+			roadTotal++;
+		}
+		if (hex.specialTerrain.includes(SpecialTerrainType.RIVER)) {
+			riverTotal++;
+		}
+		if (hex.specialTerrain.includes(SpecialTerrainType.RESOURCE)) {
+			resourceTotal++;
+		}
+	});
+
+	return (
+		mineTotal +
+		resourceTotal +
+		Math.floor(roadTotal / 4) +
+		Math.floor(riverTotal / 4)
+	);
+}
+
+export function useClaimedHexesLoyaltyBonus(): number {
+	const claimedHexes = useAppSelector((state) => selectClaimedHexes(state));
+	let aqueductTotal = 0;
+	let landmarkTotal = 0;
+
+	claimedHexes.forEach((hex) => {
+		if (hex.terrainImprovements.includes(TerrainImprovementType.AQUEDUCT)) {
+			aqueductTotal++;
+		}
+		if (hex.specialTerrain.includes(SpecialTerrainType.LANDMARK)) {
+			hex.terrainImprovements.includes(TerrainImprovementType.ROAD) ||
+			hex.terrainImprovements.includes(TerrainImprovementType.HIGHWAY)
+				? (landmarkTotal += 2)
+				: landmarkTotal++;
+		}
+	});
+
+	return aqueductTotal + landmarkTotal;
+}
+
+export function useClaimedHexesStabilityBonus(): number {
+	const claimedHexes = useAppSelector((state) => selectClaimedHexes(state));
+	let aqueductTotal = 0;
+	let fortTotal = 0;
+	let roadTotal = 0;
+	let quarryTotal = 0;
+	let sawmillTotal = 0;
+	let watchtwowerTotal = 0;
+	let lairTotal = 0;
+	let riverTotal = 0;
+
+	claimedHexes.forEach((hex) => {
+		if (hex.terrainImprovements.includes(TerrainImprovementType.AQUEDUCT)) {
+			aqueductTotal++;
+		}
+		if (hex.terrainImprovements.includes(TerrainImprovementType.FORT)) {
+			fortTotal += 2;
+		}
+		if (
+			hex.terrainImprovements.includes(TerrainImprovementType.ROAD) ||
+			hex.terrainImprovements.includes(TerrainImprovementType.HIGHWAY)
+		) {
+			roadTotal++;
+		}
+		if (hex.terrainImprovements.includes(TerrainImprovementType.QUARRY)) {
+			hex.specialTerrain.includes(SpecialTerrainType.RESOURCE)
+				? (quarryTotal += 2)
+				: quarryTotal++;
+		}
+		if (hex.terrainImprovements.includes(TerrainImprovementType.SAWMILL)) {
+			hex.specialTerrain.includes(SpecialTerrainType.RESOURCE)
+				? (sawmillTotal += 2)
+				: sawmillTotal++;
+		}
+		if (hex.terrainImprovements.includes(TerrainImprovementType.WATCHTOWER)) {
+			watchtwowerTotal++;
+		}
+		if (hex.specialTerrain.includes(SpecialTerrainType.LAIR)) {
+			lairTotal++;
+		}
+		if (hex.specialTerrain.includes(SpecialTerrainType.RIVER)) {
+			riverTotal++;
+		}
+	});
+
+	return (
+		aqueductTotal +
+		fortTotal +
+		quarryTotal +
+		sawmillTotal +
+		watchtwowerTotal +
+		lairTotal +
+		Math.floor(roadTotal / 8) +
+		Math.floor(riverTotal / 8)
+	);
+}
+
+export function useClaimedHexesConsumptionDecrease(): number {
+	const claimedHexes = useAppSelector((state) => selectClaimedHexes(state));
+	let farmTotal = 0;
+	let fisheryTotal = 0;
+	let fortTotal = 0;
+
+	claimedHexes.forEach((hex) => {
+		if (hex.terrainImprovements.includes(TerrainImprovementType.FARM)) {
+			hex.specialTerrain.includes(SpecialTerrainType.RESOURCE)
+				? (farmTotal += 3)
+				: (farmTotal += 2);
+		}
+		if (hex.terrainImprovements.includes(TerrainImprovementType.FISHERY)) {
+			hex.specialTerrain.includes(SpecialTerrainType.RESOURCE)
+				? (fisheryTotal += 2)
+				: fisheryTotal++;
+		}
+		if (hex.terrainImprovements.includes(TerrainImprovementType.FORT)) {
+			fortTotal++;
+		}
+	});
+
+	return farmTotal + fisheryTotal - fortTotal;
+}
+
+export function useClaimedHexesTerrainIncome(): number {
+	const claimedHexes = useAppSelector((state) => selectClaimedHexes(state));
+	let mineTotal = 0;
+	let quarryTotal = 0;
+	let sawmillTotal = 0;
+
+	claimedHexes.forEach((hex) => {
+		if (hex.terrainImprovements.includes(TerrainImprovementType.MINE)) {
+			hex.specialTerrain.includes(SpecialTerrainType.RESOURCE)
+				? (mineTotal += 2)
+				: mineTotal++;
+		}
+		if (hex.terrainImprovements.includes(TerrainImprovementType.QUARRY)) {
+			hex.specialTerrain.includes(SpecialTerrainType.RESOURCE)
+				? (quarryTotal += 2)
+				: quarryTotal++;
+		}
+		if (hex.terrainImprovements.includes(TerrainImprovementType.SAWMILL)) {
+			hex.specialTerrain.includes(SpecialTerrainType.RESOURCE)
+				? (sawmillTotal += 2)
+				: sawmillTotal++;
+		}
+	});
+
+	return mineTotal + quarryTotal + sawmillTotal;
 }
 
 export function mapTerrainToColor(terrain: TerrainType): string {
